39c867ae7dae552d3914aa86f759cb0cdf25eeb7
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2022-08-07 16:13:06 +0100

    updated code

---
+++ udemyTutorial.js
@@ -0,0 +1,34 @@
+/*
+DATA TYPES:
+**Data are the building blocks of any programming bc they help us organize information and determine how our programs will run
+**Comments: comments are often by developers to help clarify the meaniing of your code, it is used to explain your code and makes thing clearer and as well improve code readability
+**Strings:Strings can be any combination of characters -- letters, numbers and even emojis
+You must use matching quotes at the beginning or end of the string -- otherwise JavaScript thinks you are referring to a variable. variables should have a descriptive name of what it represents
+**Variables: with variables you no longer need to work with one-time-used data 
+when we used var keyword, we create variables that can be used in our global scope
+try to use variables that are accurate and succintly try to describe what the data is about
+**Characters:Characters within a string are indexed starting from 0, where the first character is at position 0, to n-1, where the last character is at position n-1 (n represents the total number of characters within a string).
+**Escaping Character tells JavaScript to ignore the character's special meaning and just use the literal value of the character
+**Comparing String:When you compare strings, case matters,The ASCII values of [A-Z] fall in the range [65-90], whereas,the ASCII values of [a-z] fall in the range [97-122]0
+**NOTE:you can not use "return" statement inside a forLoop for now
+**Booleans:A boolean variable is mainly essential in evaluating the outcome of conditionals (comparisons). The result of a comparison is always a boolean variable
+**In general cases (regular equality check), a true corresponds to number 1, whereas false represents a number 0. For example
+**If you purposefully assign a value to nothing...null will be returned
+**NULL refers to nothing while undefined refers to the absence of value
+**
+**LESSON THREE:CONDITIONALS
+**What good is a data if you do not put it into use
+NOTE:WHen you are writing code, the main goal is to solve a problem, the steps your code takes to solve a problem 
+known as an AN ALGORITHM.
+**FLOW CHART: this is a just a diagram for how to solve a problem.
+**A flowchart is a visual diagram that outlines the solution to a problem through a series of logical statements. The order in which statements are evaluated and executed is called the control flow.
+**If...else statements allow you to execute certain pieces of code based on a condition, or set of conditions, being met.
+**Conditionals allows you to choose ehich to code run based on the result on an expression.
+**In JavaScript we can represent complex problems by combining logical expressions with special operators called logical operators.
+**By combining two boolean values together with a logical operator, you create a logical expression that returns another boolean value.
+**Logical operators can be used to combine multiple conditional statements into a single statement.
+**Truth tables can represent the possible combinations of inputs in a logical expression
+**
+**
+**
+*/
